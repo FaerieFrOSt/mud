bef6a28d96a07b4c548ec537a567c67a4cf97a7e
adrien <adrien@debian>
2015-06-30 10:40:41 +0100

    Added client name

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from telnetServer import TelnetServer
+from player import Player
 import time
 
 server = TelnetServer("0.0.0.0", 23)
+players = {}
 
 while server.update():
     for id in server.get_new_clients():
-        print("A new client connected(" + str(id) + ")")
-        server.sendAllExcept(id, "A new client connected(" + str(id) + ")\n")
+        players[id] = Player()
+        server.send(id, "Enter your name : ")
 
     for id in server.get_disconnected_clients():
-        print("A client disconnected(" + str(id) + ")")
-        server.sendAllExcept(id, "A client disconnected(" + str(id) + ")\n")
+        name = str(id)
+        try:
+            name = players[id].name
+            if not name:
+                name = str(id)
+            del(players[id])
+        except KeyError:
+            pass
+        if name not in str(id):
+            server.sendAll(name + " disconnected\n")
+
     for id, message in server.get_messages():
-        print(str(id) + " sent : " + message)
-        server.sendAll(str(id) + " sent : " + message + "\n")
+        try:
+            if not players[id].name:
+                players[id].name = message
+                server.sendAll(message + " has connected\n")
+                continue
+            server.sendAll(players[id].name + " sent : " + message + "\n")
+        except KeyError:
+            pass
     time.sleep(0.2)
---
+++ player.py
@@ -0,0 +1,10 @@
+class   Player():
+    def __init__(self):
+        self.name = None
+
+    def load(self, name, password):
+        return False
+
+    def Create(self, name, password):
+        self.name = name
+        return True
