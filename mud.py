2003198515f9a160722761d3ba27edb6911d4f2d
adrien <adrien@debian>
2015-07-01 17:50:18 +0100

    Added mud data in XML

---
+++ mud.py
@@ -0,0 +1,24 @@
+import xml.etree.ElementTree as etree
+
+class   Mud():
+    class   Room():
+        def __init__(self, name, desc, exits):
+            self.name = name
+            self.desc = desc
+            self.exits = exits
+
+    def __init__(self, file):
+        tree = etree.parse(file)
+        root = tree.getroot()
+        self.rooms = []
+        for child in root:
+            exits = []
+            for j in range(2, len(child)):
+                exits.append(child[j].text)
+            self.rooms.append(Mud.Room(child[0].text, child[1].text, exits))
+
+    def print_rooms(self):
+        for i in self.rooms:
+            print(i.name)
+            print(i.desc)
+            print(i.exits)
