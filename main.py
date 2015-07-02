526cc68399211934f6f6714728d6d56dde648390
adrien <adrien@debian>
2015-07-02 12:37:04 +0100

    Added spacialization

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from telnetServer import TelnetServer
 from player import Player
+from mud import Mud
 import time
+
+def getPlayersInRoom(players, room):
+    for i in players:
+        if i.room in room:
+            yield i
 
 server = TelnetServer("0.0.0.0", 23)
 players = {}
+mud = Mud("data.xml")
+if not mud.begin:
+    exit()
 
 while server.update():
     for id in server.get_new_clients():
@@ -25,10 +34,20 @@ while server.update():
     for id, message in server.get_messages():
         try:
             if not players[id].name:
-                players[id].name = message
+                players[id].create(message, mud.begin)
                 server.sendAll(message + " has connected\n")
+                try:
+                    server.send(id, "You are in " + mud.rooms[players[id].room].name, + "\n")
+                except KeyError:
+                    players[id].room = mud.begin
+                    server.send(id, "You have been teleported back to " + mud.begin + "\n")
+                if not players[id].isVisited(mud.begin):
+                    players[id].visit(mud.begin)
+                    server.send(id, mud.rooms[mud.begin].desc + "\n")
                 continue
-            server.sendAll(players[id].name + " sent : " + message + "\n")
+            for i in getPlayersInRoom(players, players[id].room):
+                server.send(i.name + " sent : " + message + "\n")
         except KeyError:
-            pass
+            server.send(id, "You have to log in before\n")
+            server.disconnect(id)
     time.sleep(0.2)
