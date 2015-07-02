bd5f0b0eadab6368fd09effcfed1d29412503322
adrien <adrien@debian>
2015-07-02 12:36:35 +0100

    Added visited rooms to skip the desc

--- player.py
+++ player.py
@@ -2,10 +2,18 @@ class   Player():
     def __init__(self):
         self.name = None
         self.room = None
+        self.visited = []
 
-    def load(self, name, password):
+    def load(self, name):
         return False
 
-    def Create(self, name, password):
+    def create(self, name, room):
         self.name = name
+        self.room = room
         return True
+
+    def visit(self, room):
+        self.visited.append(room)
+
+    def isVisited(self, room):
+        return room in self.visited
