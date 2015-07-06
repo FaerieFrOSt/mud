c4b527cc3f1f890e94220e793fb4e1a79f5dd0f9
adrien <adrien@debian>
2015-07-06 10:13:57 +0100

    Updated send function for worker

--- worker.py
+++ worker.py
@@ -13,35 +13,42 @@ class   Worker():
                 'tell' : self.tell,
                 'quit' : self.quit,
                 }
+
     def help(self, player, message):
         data = """Here is the list of the commands :
         help - print this help,
         say  - say something,
         tell - tell someone something
         quit - quit the mud\n"""
-        self.send(data, to=[player])
+        self.send(data, to=player)
 
     def say(self, player, message):
         data = player.name + " says '" + ' '.join(message[1:]) + "'\n"
-        self.send(data, dont=[player], rooms=[player.room])
+        self.send(data, dont=player, rooms=player.room)
         data = "You say '" + ' '.join(message[1:]) + "'\n"
-        self.send(data, to=[player])
+        self.send(data, to=player)
 
     def quit(self, player, message):
         try:
             id = player.id
-            self.send(player.name + " has disconnected\n", dont=[player],
-                    rooms=[player.room])
-            self.send("See you soon!\n", to=[player])
+            self.send(player.name + " has disconnected\n", dont=player,
+                    rooms=player.room)
+            self.send("See you soon!\n", to=player)
             del(self.players[id])
             self.server.disconnect(id)
         except KeyError:
             pass
 
     def tell(self, player, message):
-        self.send("Not implemented\n", to=[player])
+        self.send("Not implemented\n", to=player)
 
     def send(self, message, to=[], dont=[], rooms=[]):
+        if not isinstance(to, list):
+            to = [to]
+        if not isinstance(dont, list):
+            dont = [dont]
+        if not isinstance(rooms, list):
+            rooms = [rooms]
         for i in to:
             if i in dont:
                 continue
@@ -55,7 +62,7 @@ class   Worker():
     def connections(self):
         for id in self.server.get_new_clients():
             self.players[id] = Player(id)
-            self.send("Enter your name : ", to=[self.players[id]])
+            self.send("Enter your name : ", to=self.players[id])
 
     def disconnections(self):
         for id in self.server.get_disconnected_clients():
@@ -63,8 +70,8 @@ class   Worker():
             try:
                 player = self.players[id]
                 del(self.players[id])
-                self.send(player.name + " disconnected\n", dont=[player],
-                        room=[player.room])
+                self.send(player.name + " disconnected\n", dont=player,
+                        room=player.room)
             except KeyError:
                 pass
 
@@ -78,16 +85,16 @@ class   Worker():
 
     def sendRoom(self, player):
         self.send("You are in " + self.getRoom(player).name + "\n",
-                to=[player])
+                to=player)
 
     def sendDesc(self, player):
         if player.isVisited(player.room):
             return
         player.visit(player.room)
-        self.send(self.getRoom(player).desc + "\n", to=[player])
+        self.send(self.getRoom(player).desc + "\n", to=player)
 
     def sendCommandLine(self, player):
-        self.send(">", to=[player])
+        self.send(">", to=player)
 
     def loadOrCreate(self, id, message):
         player = self.players[id]
@@ -99,12 +106,12 @@ class   Worker():
             if not player.isVisited(player.room):
                 self.sendDesc(player)
                 player.visit(player.room)
-            self.send(player.name + " has connected\n", dont=[player],
-                    rooms=[player.room])
+            self.send(player.name + " has connected\n", dont=player,
+                    rooms=player.room)
         except KeyError:
             player.room = self.mud.begin
             self.send("You have been teleported back to " + player.room
-                    + "\n", to=[player])
+                    + "\n", to=player)
         return player, False
 
     def messages(self):
@@ -116,7 +123,7 @@ class   Worker():
                     self.sendCommandLine(player)
                     continue
             except KeyError:
-                self.send(id, "You have to log in before\n", to=[player])
+                self.send(id, "You have to log in before\n", to=player)
                 self.server.disconnect(id)
             if not player:
                 continue
@@ -134,4 +141,4 @@ class   Worker():
                 self.sendCommandLine(player)
             except KeyError:
                 self.send("This is not a known command, try again\n",
-                        to=[player])
+                        to=player)
