ac9e0b6b31cd8f0ad80e648b6e110979471dc978
Faerie <Faerie@test.com>
2015-07-09 12:08:04 +0100

    Added mysql wrapper

---
+++ mysql.py
@@ -0,0 +1,26 @@
+import MySQLdb as mdb
+
+class   Mysql():
+    def __init__(self, address, db, name, password):
+        self.ok = True
+        try:
+            self.con = mdb.connect(address, name, password, db)
+            cur = self.con.cursor()
+            cur.execute("select version()")
+            ver = cur.fetchone()
+        except mdb.Error, e:
+            self.ok = False
+
+    def getEntry(self, command):
+        if not self.ok:
+            yield None
+        cur = self.con.cursor(mdb.cursors.DictCursor)
+        cur.execute(command)
+        for i in range(cur.rowcount):
+            yield cur.fetchone()
+
+    def writeEntry(self, command):
+        if not self.ok:
+            return
+        cur = self.con.cursor()
+        cur.execute(command)
